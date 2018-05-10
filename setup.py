a3e6200d039f49029424c20bddbe48b6d127a5cb
Michael Kelly <mkelly@mozilla.com>
2018-05-10 14:59:10 -0700

    Remove setup.py-based requirements, since Docker already uses requirements.txt.

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@ setup(
     name='bec-alerts',
     version='0.1.0',
     py_modules=['bec_alerts'],
-    install_requires=[
-        'Click',
-        'boto3',
-        'Django',
-        'python-dotenv',
-    ],
     entry_points='''
         [console_scripts]
         bec-alerts=bec_alerts.cli:cli
